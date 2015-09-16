ca76d63330a512dfbcc754b3e95df5bacaf5673c
febyola <febyolatiar@gmail.com>
2015-09-16 07:10:07 +0000

    show all item

--- lists/tests.py
+++ lists/tests.py
@@ -6,33 +6,96 @@ from lists.views import home_page
 from lists.models import Item
 
 # Create your tests here.
-#class HomePageTest(TestCase):
-#	def test_root_url_resolves_to_home_page_view(self):
-#		found = resolve('/')
-#		self.assertEqual(found.func, home_page)
+class HomePageTest(TestCase):
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
 
-#	def test_home_page_returns_correct_html(self):
-#		request = HttpRequest()
-#		response = home_page(request)
-#		#self.assertTrue(response.content.startswith(b'<html>'))
-#		#self.assertIn(b'<title>To-Do lists</title>', response.content)
-#		#self.assertTrue(response.content.strip().endswith(b'</html>'))
-#		expected_html = render_to_string('home.html')
-#		self.assertEqual(response.content.decode(), expected_html)
-#	
-#	def test_home_page_can_save_a_POST_request(self):
-#		request = HttpRequest()
-#		request.method = 'POST'
-#		request.POST['item_text'] = 'A new list item'
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
 
-#		response = home_page(request)
+		expected_html = render_to_string('home.html',
+			{ 'item_label': 'yey, waktunya berlibur' }
+		)
+		self.assertEqual(response.content.decode(), expected_html)
+		
+		#self.assertTrue(response.content.startswith(b'<html>'))
+		#self.assertIn(b'<title>To-Do lists</title>', response.content)
+		#self.assertTrue(response.content.strip().endswith(b'</html>'))
+		
+		#expected_html = render_to_string('home.html')
+		#self.assertEqual(response.content.decode(), expected_html)
 	
-#		self.assertIn('A new list item', response.content.decode())
-#		expected_html = render_to_string(
-#			'home.html',
-#			{'new_item_text': 'A new list item'}
-#		)
-#		self.assertEqual(response.content.decode(), expected_html)
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+		
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+		
+	def test_home_page_redirect_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		
+		response = home_page(request)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_only_saves_items_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
+	def test_check_zero_item(self):
+		#Item.objects.create(text='item1')
+
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+		self.assertIn('yey, waktunya berlibur', response.content.decode())
+	
+	def test_check_less_five_item(self):
+		Item.objects.create(text='itemy 1')
+		#Item.objects.create(text='itemy 2')
+		#Item.objects.create(text='itemy 3')
+		#Item.objects.create(text='itemy 4')
+		#Item.objects.create(text='itemy 5')
+
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertLess(Item.objects.count(), 5)
+		self.assertIn('sibuk tapi santai', response.content.decode())
+		
+	def test_check_greater_equal_five_items(self):
+		Item.objects.create(text='itemy 1')
+		Item.objects.create(text='itemy 2')
+		Item.objects.create(text='itemy 3')
+		Item.objects.create(text='itemy 4')
+		Item.objects.create(text='itemy 5')
+
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertGreaterEqual(Item.objects.count(), 5)
+		self.assertIn('oh tidak', response.content.decode())
+
+
 
 class ItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
@@ -45,9 +108,18 @@ class ItemModelTest(TestCase):
 		second_item.save()
 
 		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(),2)
+		self.assertEqual(saved_items.count(), 2)
 
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
+
+	#def test_check_zero_item(self):
+	#	self.assertEqual(Item.objects.count(),0)
+		
+	#def test_check_less_five_items(self):
+	#	self.assertLess(Item.objects.count(), 5)
+
+	#def test_check_greater_equal_five_items(self):
+	#	self.assertGreaterEqual(Item.objects.count(), 5)
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+#from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	#if request.method == 'POST':
-		#return HttpResponse(request.POST['item_text'])
-	return render(request, 'home.html', {
-		 'new_item_text': request.POST.get('item_text', ''),
-	})
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+	
+	items = Item.objects.all()
+	
+	item_label = ''
+	if Item.objects.count() == 0:
+		item_label = 'yey, waktunya berlibur'
+	elif Item.objects.count() < 5:
+		item_label = 'sibuk tapi santai'
+	elif Item.objects.count() >= 5:
+		item_label = 'oh tidak'
+	return render(request, 'home.html', {'items': items, 'item_label': item_label})
