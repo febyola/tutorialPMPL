6cd7752dc85ddb83c2b9a256289c1013687aaff5
febyola <febyolatiar@gmail.com>
2015-10-28 09:41:59 +0000

    hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,24 @@
-#from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
-
-#class NewVisitorTest(LiveServerTestCase):
 class NewVisitorTest(StaticLiveServerTestCase):
 	
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://'+ arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -23,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 #		self.browser.get('http://localhost:8081')
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -53,7 +65,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser= webdriver.Firefox()
 
 #Francis visits the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -77,7 +89,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	
 	def test_layout_and_styling(self):
 		#edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		#she notice the input box is nicely centered
